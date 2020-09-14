81bc5086feec6ded4c5f80b71ba8217b6295d9c6
kelvinsanchez15 <kelvinsanchez15@gmail.com>
2020-09-14 19:39:06 -0400

    add responsiveFontSizes function

--- components/styles/theme.js
+++ components/styles/theme.js
@@ -1,22 +1,24 @@
-import { createMuiTheme } from '@material-ui/core/styles';
-import { red } from '@material-ui/core/colors';
+import { createMuiTheme, responsiveFontSizes } from "@material-ui/core/styles";
+import { red } from "@material-ui/core/colors";
 
 // Create a theme instance.
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: '#556cd6',
+const theme = responsiveFontSizes(
+  createMuiTheme({
+    palette: {
+      primary: {
+        main: "#556cd6",
+      },
+      secondary: {
+        main: "#19857b",
+      },
+      error: {
+        main: red.A400,
+      },
+      background: {
+        default: "#fff",
+      },
     },
-    secondary: {
-      main: '#19857b',
-    },
-    error: {
-      main: red.A400,
-    },
-    background: {
-      default: '#fff',
-    },
-  },
-});
+  })
+);
 
 export default theme;
