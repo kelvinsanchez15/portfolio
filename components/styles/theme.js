bdc7427327cf367eef5df65889527f68d7829c66
kelvinsanchez15 <kelvinsanchez15@gmail.com>
2020-09-15 10:31:59 -0400

    set dark mode theme

--- components/styles/theme.js
+++ components/styles/theme.js
@@ -1,22 +1,12 @@
 import { createMuiTheme, responsiveFontSizes } from "@material-ui/core/styles";
-import { red } from "@material-ui/core/colors";
+import blue from "@material-ui/core/colors/blue";
 
 // Create a theme instance.
 const theme = responsiveFontSizes(
   createMuiTheme({
     palette: {
-      primary: {
-        main: "#556cd6",
-      },
-      secondary: {
-        main: "#19857b",
-      },
-      error: {
-        main: red.A400,
-      },
-      background: {
-        default: "#fff",
-      },
+      type: "dark",
+      primary: blue,
     },
   })
 );
