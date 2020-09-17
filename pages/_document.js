a4a06e3164675c2dfd7dcae59ff8ba3173bceabb
kelvinsanchez15 <kelvinsanchez15@gmail.com>
2020-09-17 09:30:29 -0400

    add custom favicon

--- pages/_document.js
+++ pages/_document.js
@@ -14,6 +14,26 @@ export default class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <meta name="msapplication-config" content="/browserconfig.xml" />
+          <meta name="msapplication-TileColor" content="#ffffff" />
         </Head>
         <body>
           <Main />
