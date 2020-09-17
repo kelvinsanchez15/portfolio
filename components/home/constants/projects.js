41f73f57ed56368e287f0c62e62b3903ec1fecaa
kelvinsanchez15 <kelvinsanchez15@gmail.com>
2020-09-17 10:55:38 -0400

    refactor projects const into a individual file on a constants directory

---
+++ components/home/constants/projects.js
@@ -0,0 +1,59 @@
+const projects = [
+  {
+    name: "Anonymous Message Board",
+    projectUrl: "https://anonymous-message-board-kel.glitch.me",
+    repoUrl: "https://github.com/kelvinsanchez15/anonymous-message-board",
+    imgPath: "projectMedia/anonymous-message-board.jpg",
+    imgAlt: "Anonymous message board preview",
+    summary:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    keyFeatures: [
+      "CRUD API",
+      "Server side rendering",
+      "Flash alerts",
+      "Quality assurance",
+      "Dark theme",
+    ],
+    technologies: ["Express", "Node", "MongoDB", "PUG"],
+  },
+  {
+    name: "Issue Tracker",
+    projectUrl: "https://issue-tracker-kel.glitch.me",
+    repoUrl: "https://github.com/kelvinsanchez15/issue-tracker",
+    imgPath: "projectMedia/issue-tracker.jpg",
+    imgAlt: "Issue tracker preview",
+    summary:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    keyFeatures: ["CRUD API", "Filters", "Relative time", "Test enviroment"],
+    technologies: ["Express", "Node", "MongoDB", "PUG"],
+  },
+  {
+    name: "Choropleth Map",
+    projectUrl: "https://anonymous-message-board-kel.glitch.me",
+    repoUrl: "https://github.com/kelvinsanchez15/choropleth-map",
+    imgPath: "projectMedia/choropleth-map.jpg",
+    imgAlt: "Choropleth map preview",
+    summary:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    keyFeatures: [
+      "Data visualization",
+      "Color legend",
+      "Data tooltip",
+      "Dark theme",
+    ],
+    technologies: ["Vanilla JS", "D3"],
+  },
+  {
+    name: "JavaScript Calculator",
+    projectUrl: "https://anonymous-message-board-kel.glitch.me",
+    repoUrl: "https://github.com/kelvinsanchez15/javascript-calculator",
+    imgPath: "projectMedia/javascript-calculator.jpg",
+    imgAlt: "JavaScript calculator preview",
+    summary:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    keyFeatures: ["Feature 1", "Feature 2", "Feature 3"],
+    technologies: ["React"],
+  },
+];
+
+export default projects;
