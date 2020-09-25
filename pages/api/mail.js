1dab0ea87f0883f11fa95ef2b531c615697deb5b
kelvinsanchez15 <kelvinsanchez15@gmail.com>
2020-09-25 16:28:31 -0400

    fix send email serverless function

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -3,7 +3,7 @@ const sgMail = require("@sendgrid/mail");
 export default async (req, res) => {
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-  try {
+  if (req.method === "POST") {
     const { firstName, lastName, email, message } = req.body;
 
     const msg = {
@@ -11,16 +11,20 @@ export default async (req, res) => {
       from: process.env.FROM_EMAIL,
       subject: `PORTFOLIO: ${firstName} ${lastName} sent you a message!`,
       text: `
-        Email: ${email}
-        Message: ${message}`,
+          Email: ${email}
+          Message: ${message}`,
     };
 
-    await sgMail.send(msg);
+    try {
+      await sgMail.send(msg);
 
-    console.log("Message sent");
-    res.status(200).end();
-  } catch (error) {
-    console.log("ERROR SENDING EMAIL VIA SENDGRID: ", error);
-    res.status(500).json(error.message);
+      console.log("Message sent");
+      res.status(200).end();
+    } catch (error) {
+      console.log("ERROR SENDING EMAIL VIA SENDGRID: ", error);
+      res.status(500).json(error.message);
+    }
   }
+
+  res.status(200).end();
 };
