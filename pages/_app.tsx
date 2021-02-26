02e8ce21b2752a5b5f53976b6657d2d5f5c5a307
Kelvin Sanchez <kelvinsanchez15@gmail.com>
2021-02-26 10:23:43 -0400

    migrate file to typescript

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,35 +1,32 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import theme from '../components/styles/theme';
 
+import theme from '../components/styles/theme';
 import Navbar from '../components/layout/Navbar';
 import Footer from '../components/layout/Footer';
 
-export default function MyApp(props) {
-  const { Component, pageProps } = props;
-
+export default function MyApp({ Component, pageProps }: AppProps) {
   React.useEffect(() => {
     // Remove the server-side injected CSS.
     const jssStyles = document.querySelector('#jss-server-side');
     if (jssStyles) {
-      jssStyles.parentElement.removeChild(jssStyles);
+      jssStyles.parentElement?.removeChild(jssStyles);
     }
   }, []);
 
   return (
     <>
       <Head>
-        <title>My page</title>
+        <title>Kelvin Sánchez Portfolio</title>
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
         />
       </Head>
       <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
         <Navbar />
         <Component {...pageProps} />
@@ -38,8 +35,3 @@ export default function MyApp(props) {
     </>
   );
 }
-
-MyApp.propTypes = {
-  Component: PropTypes.elementType.isRequired,
-  pageProps: PropTypes.object.isRequired,
-};
