5527770900a4409b3e5e99520dbaa9044aaf27eb
Kelvin Sanchez <kelvinsanchez15@gmail.com>
2021-02-28 18:27:18 -0400

    migrate file to typescript

--- components/EmailSuccessMessage.tsx
+++ components/EmailSuccessMessage.tsx
@@ -1,11 +1,21 @@
+import React from 'react';
 import { Snackbar } from '@material-ui/core';
+
+interface EmailSuccessMessageProps {
+  senderFirstName: string;
+  displayMessage: boolean;
+  setDisplayMessage: React.Dispatch<React.SetStateAction<boolean>>;
+}
 
 export default function EmailSuccessMessage({
   senderFirstName,
-  setDisplayMessage,
   displayMessage,
-}) {
-  const handleClose = (event, reason) => {
+  setDisplayMessage,
+}: EmailSuccessMessageProps) {
+  const handleClose = (
+    event: React.SyntheticEvent | React.MouseEvent,
+    reason?: string
+  ) => {
     if (reason === 'clickaway') {
       return;
     }
