867fa5820f178d807d5a97698857fce2219c5673
Kelvin <kelvinsanchez15@gmail.com>
2023-11-04 16:42:23 -0500

    bugfix: update broken links

--- components/home/constants/blogs.ts
+++ components/home/constants/blogs.ts
@@ -4,7 +4,7 @@ const blogs = {
     category: 'NEXTJS',
     link: 'https://www.freecodecamp.org/espanol/news/aprende-next-js-desde-cero/',
     mediaSrc:
-      'https://res.cloudinary.com/demo/image/fetch/https://www.freecodecamp.org/espanol/news/content/images/2021/01/next-js-logo1.png',
+      'https://www.freecodecamp.org/espanol/news/content/images/2021/01/next-js-logo1.png',
     description: `Next.js es un framework de JavaScript que nos permite crear fácilmente sitios web de React listos para salir a producción.
 
     En este artículo profundizaremos en la documentación oficial, desglosando los conceptos básicos, funciones y particularidades de Next.js.`,
@@ -15,7 +15,7 @@ const blogs = {
       category: 'PWA',
       link: 'https://www.freecodecamp.org/espanol/news/como-construir-una-pwa-desde-cero-con-html-css-y-javascript/',
       mediaSrc:
-        'https://res.cloudinary.com/demo/image/fetch/https://www.freecodecamp.org/espanol/news/content/images/2020/12/Group-1.png',
+        'https://www.freecodecamp.org/espanol/news/content/images/2020/12/Group-1.png',
     },
     {
       title:
@@ -23,21 +23,21 @@ const blogs = {
       category: 'JAVASCRIPT',
       link: 'https://www.freecodecamp.org/espanol/news/ordenar-arreglos-en-javascript-como-usar-el-metodo-sort/',
       mediaSrc:
-        'https://res.cloudinary.com/demo/image/fetch/https://images.unsplash.com/photo-1522252234503-e356532cafd5?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxMTc3M3wwfDF8c2VhcmNofDc0fHxjb2RlJTIwc29ydHxlbnwwfHx8&ixlib=rb-1.2.1&q=80&w=2000',
-    },
+      'https://www.freecodecamp.org/espanol/news/content/images/size/w2000/2021/04/photo-1522252234503-e356532cafd5.jpg'
+      },
     {
       title: 'Una guía rápida para cambiar tu nombre de usuario en GitHub',
       category: 'GITHUB',
       link: 'https://www.freecodecamp.org/espanol/news/una-guia-rapida-para-cambiar-tu-nombre-de-usuario-en-github/',
       mediaSrc:
-        'https://res.cloudinary.com/demo/image/fetch/https://www.freecodecamp.org/espanol/news/content/images/size/w300/2020/12/barcat.png',
+        'https://www.freecodecamp.org/espanol/news/content/images/size/w300/2020/12/barcat.png',
     },
     {
       title: 'Subir a GitHub - lo suficientemente simple para Poetas',
       category: 'GIT',
       link: 'https://www.freecodecamp.org/espanol/news/subir-a-github-lo-suficientemente-simple-para-poetas/',
       mediaSrc:
-        'https://res.cloudinary.com/demo/image/fetch/https://images.unsplash.com/photo-1561164517-686f490ee86d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxMTc3M3wwfDF8c2VhcmNofDR8fGdpdGh1YnxlbnwwfHx8&ixlib=rb-1.2.1&q=80&w=2000',
+      'https://www.freecodecamp.org/espanol/news/content/images/size/w2000/2021/04/photo-1561164517-686f490ee86d.jpg'
     },
   ],
 };
